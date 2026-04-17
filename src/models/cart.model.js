9af0dc2cf602689d8b7fc21a55500749c9e317e2
LeGiaBao2109 <giabao.yugioh@gmail.com>
2026-04-17 14:45:57 +0700

    Add cart model

---
+++ src/models/cart.model.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const cartItemSchema = new mongoose.Schema({
+    productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "products",
+        required: true
+    },
+    sku: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        default: 1,
+        min: 1
+    }
+}, { _id: false });
+
+const cartSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+        default: null
+    },
+
+    items: {
+        type: [cartItemSchema],
+        default: []
+    },
+
+    totalAmount: {
+        type: Number,
+        default: 0
+    },
+
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model("carts", cartSchema);
